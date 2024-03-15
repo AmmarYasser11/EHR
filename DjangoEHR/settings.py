51b9759fb4b86b63b484f3696365cefcb60da783
AmmarYasser11 <3ammar.sabry@gmail.com>
2024-03-15 22:46:38 +0200

    SecurityUpdate

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'lab3database',
-        'PASSWORD': 'Mypassword3',
-        'HOST': 'ehrserver.postgres.database.azure.com',
+        'USER': 'Ammar',
+        'PASSWORD': 'Ajaxjuve07.',
+        'HOST': 'ammar-yasser74.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS':{'sslmode':'require'},
     }
